81564966c9b8792fb4cc570142612d8c2c1597f8
Guido-PMC <101594840+Guido-PMC@users.noreply.github.com>
2022-05-03 15:12:55 -0300

    Add files via upload

--- middleware_flask.py
+++ middleware_flask.py
@@ -5,11 +5,35 @@ import json
 import os
 
 app = Flask(__name__)
-@app.route('/', methods = ['GET', 'POST'])
-def wallet():
+zabbix_address = os.environ['ZABBIX']
+print("Zabbix Adress: "+zabbix_address)
+cantidad_envios={}
+diccionario={}
+salida={}
+@app.route('/wallet/<wallet_id>', methods = ['GET', 'POST'])
+def wallet(wallet_id):
+    if request.method == 'GET':
+        for x in diccionario:
+            stream = os.popen("zabbix_sender -z "+zabbix_address+"    -s '"+str(x)+"' -k application.hash -o '"+str(diccionario[x])+"'")
+            output = stream.read()
+            print("Wallet: "+str(x)+" Hash: "+str(diccionario[x])+" Cantidad envios: "+str(cantidad_envios[x]))
+            salida[x] = diccionario[x]
+            diccionario[x] = int(0)
+            cantidad_envios[x] = int(0)
+        return salida
     if request.method == 'POST':
         data = request.json # a multidict containing POST data
-        print(data)
-        return str(data)
+        try:
+            diccionario[wallet_id] = int(diccionario[wallet_id]) + int(data["value"])
+            print("Wallet: "+wallet_id+" Hash: "+data["value"])
+        except Exception as e:
+            diccionario[wallet_id] = int(data["value"])
+            print("Wallet: "+wallet_id+" Hash: "+data["value"])
+        try:
+            cantidad_envios[wallet_id] = int(cantidad_envios[wallet_id]) + 1
+        except Exception as e:
+            cantidad_envios[wallet_id] = 1
+
+        return str(diccionario[wallet_id])
 if __name__ == '__main__':
    app.run(host= '0.0.0.0', port=2000)
