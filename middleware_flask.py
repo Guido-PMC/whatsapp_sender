1fe1ab35687237da6f00554f5eda266bba2655dd
guigonzalez <guidogermang@gmail.com>
2022-05-12 16:49:04 -0300

    Cambio formato de Excel

--- middleware_flask.py
+++ middleware_flask.py
@@ -47,7 +47,7 @@ def test():
         data = request.json # a multidict containing POST data
         print("Imprimo la data \n\n")
         print(data)
-        new_json = {"nombre": data["nombre"],"mail":data["email"], "celular":data["tel"], "servicio":data["servicio"],"detalle":data["rig-tipo-presupuesto"]+" "+data["rig-tipo-dinero"]+" "+data["rig-tipo-rentabilidad"]+" "+data["rig-tipo-megahash"]+" "+data["housing-placas"]+" "+data["housing-rigs"]+" "+data["exchange-operacion"]+" "+data["exchange-monto"] }
+        new_json = {"nombre": data["nombre"],"mail":data["email"], "celular":data["tel"], "servicio":data["servicio"],"detalle":data["rig-tipo-presupuesto"]+" "+data["rig-tipo-dinero"]+" "+data["rig-tipo-rentabilidad"]+" "+data["rig-tipo-megahash"]+" "+data["housing-placas"]+" "+data["housing-rigs"]+" "+data["exchange-operacion"]+" "+data["exchange-monto"]+" "+data["mensaje"] }
         row = list(new_json.values())
         append_row("Contactos WEB - PMC", "Hoja 1", row)
         data = {'name': 'Guido'}
