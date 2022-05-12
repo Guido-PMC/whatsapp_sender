c85da7e64be1f3593aac850d465c7676cc94e3b9
guigonzalez <guidogermang@gmail.com>
2022-05-12 16:16:31 -0300

    Cambio formato de Excel

--- middleware_flask.py
+++ middleware_flask.py
@@ -7,6 +7,24 @@ from oauth2client.service_account import ServiceAccountCredentials
 import json
 import os
 
+
+class formulario:
+    def __init__(self, servicio, rig_tipo_presupuesto, rig_tipo_dinero, rig_tipo_rentabilidad, rig_tipo_megahash, housing_placas, housing_rigs, exchange_operacion, exchange_monto, nombre, email, tel, mensaje):
+        self.servicio = servicio
+        self.rig_tipo_presupuesto = rig_tipo_presupuesto
+        self.rig_tipo_dinero = rig_tipo_dinero
+        self.rig_tipo_rentabilidad = rig_tipo_rentabilidad
+        self.rig_tipo_megahash = rig_tipo_megahash
+        self.housing_placas = housing_placas
+        self.housing_rigs = housing_rigs
+        self.exchange_operacion = exchange_operacion
+        self.exchange_monto = exchange_monto
+        self.nombre = nombre
+        self.email = email
+        self.tel = tel
+        self.mensaje = mensaje
+
+
 if "TEST" in os.environ['ENVIRONMENT']:
     credenciales = 'pilarminingco-c11e8da70b2f.json'
 if "MACBOOK" in os.environ['ENVIRONMENT']:
@@ -27,8 +45,10 @@ app = Flask(__name__)
 def test():
     if request.method == 'POST':
         data = request.json # a multidict containing POST data
+        print("Imprimo la data \n\n")
         print(data)
-        row = list(filter(lambda i:i!="", data.values()))
+        new_json = {"nombre": data["nombre"],"mail":data["email"], "celular":data["tel"], "servicio":data["servicio"],"detalle":data["rig-tipo-presupuesto"]+" "+data["rig-tipo-dinero"]+" "+data["rig-tipo-rentabilidad"]+" "+data["rig-tipo-megahash"]+" "+data["housing-placas"]+" "+data["housing-rigs"]+" "+data["exchange-operacion"]+" "+data["exchange-monto"] }
+        row = list(new_json.values())
         append_row("Contactos WEB - PMC", "Hoja 1", row)
         data = {'name': 'Guido'}
         return jsonify(data)
